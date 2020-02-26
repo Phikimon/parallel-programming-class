9cc9439c555def7210c850325381bfd43de9ec57
Filipp Mikoian <Filipp.Mikoian@acronis.com>
2020-02-26 18:43:23 -0500

    S1T1: add simple test

---
+++ seminar_1/program_1/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+make > /dev/null
+./main 1000 > output_1000
+seq 1 1000 > seq_1000
+if [ "`cmp seq_1000 output_1000`" ]
+then
+	echo "Basic test not passed:"
+	cmp seq_1000 output_1000
+else
+	echo "Basic test passed"
+	make clean > /dev/null
+fi
