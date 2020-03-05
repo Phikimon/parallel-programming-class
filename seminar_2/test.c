da42f33f784d0e8f9628dfb513108a9c2c66cc35
Filipp Mikoian <Filipp.Mikoian@acronis.com>
2020-03-04 23:52:39 -0500

    S2: adapt test for multiple threads

--- seminar_2/test.c
+++ seminar_2/test.c
@@ -1,8 +1,11 @@
+#define _DEFAULT_SOURCE
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <assert.h>
 #include <stdint.h>
 #include <limits.h>
+#include <pthread.h>
 
 #include "list.h"
 
@@ -19,7 +22,7 @@ struct thread_arg {
 	int id;
 };
 
-void grow_list(void* arg);
+void* grow_list(void* arg);
 void print_list(struct my_struct* head);
 int verify_list(struct my_struct* head, long thread_num, int list_quota);
 
@@ -27,6 +30,7 @@ long read_long(long* res, const char* str);
 
 int main(int argc, char* argv[])
 {
+	// Read command line arguments
 	long list_size, thread_num;
 	if (argc != 3) {
 		fprintf(stderr, "usage: ./test <list_size> <thread_num>\n");
@@ -41,9 +45,10 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
+	// Prepare arguments for threads
 	int list_quota = (int)list_size / thread_num;
 	if (list_quota == 0) {
-		fprintf(stderr, "Thread number(%ld) exceeds list size(%d)\n", thread_num, list_size);
+		fprintf(stderr, "Thread number(%ld) exceeds list size(%d)\n", thread_num, (int)list_size);
 		return 1;
 	}
 
@@ -53,20 +58,37 @@ int main(int argc, char* argv[])
 		.list = LIST_HEAD_INIT(head.list),
 	};
 
-	struct thread_arg targ = {
-		.head = &head,
-		.list_quota = list_quota,
-		.id = 0,
-	};
+	// Allocate memory for threads' stuff
+	pthread_t* thr =(pthread_t*)calloc(thread_num, sizeof(*thr));
+	assert(thr);
+	struct thread_arg* thr_args = (struct thread_arg*)calloc(thread_num, sizeof(*thr_args));
+	assert(thr_args);
 
-	grow_list(&targ);
-	print_list(&head);
+	int r;
+	// Start threads
+	for (int i = 0; i < thread_num; i++) {
+		thr_args[i].head = &head;
+		thr_args[i].list_quota = list_quota;
+		thr_args[i].id = i;
+		r = pthread_create(&thr[i], NULL, grow_list, &thr_args[i]);
+		assert((r == 0) && "pthread_create");
+	}
+
+	// Wait until all threads exit
+	for (int i = 0; i < thread_num; i++) {
+		r = pthread_join(thr[i], NULL);
+		assert((r == 0) && "pthread_join");
+	}
+
+	// Print if not too big
+	if (list_size <= 30)
+		print_list(&head);
 
 	return verify_list(&head, thread_num, list_quota);
 }
 
 // Thread's func. Adds 'list_quota' elements to the list
-void grow_list(void* arg) {
+void* grow_list(void* arg) {
 	struct thread_arg* targ = (struct thread_arg*)arg;
 	int num = targ->list_quota;
 	struct my_struct* head = targ->head;
@@ -75,15 +97,21 @@ void grow_list(void* arg) {
 	struct my_struct* sublist = (struct my_struct*)calloc(num, sizeof(*sublist));
 	assert(sublist);
 
-	struct list_head* prev = &head->list;
 	for (int i = 0; i < num; i++) {
 		sublist[i].owner = id;
-		sublist[i].index = i;
-		list_add(&sublist[i].list, prev);
-		prev = &sublist[i].list;
+		// Elements will be added right in front of the head, since
+		// it creates more contention which is good. As a result
+		// they will be added to the list in reverse order, so
+		// we reverse index order as well.
+		sublist[i].index = num - 1 - i;
+		list_add(&sublist[i].list, &head->list);
+		usleep(1);
 	}
+
+	return NULL;
 }
 
+// Verifies elements' order and amount for each owner
 int verify_list(struct my_struct* head, long thread_num, int list_quota)
 {
 	struct my_struct* pos;
@@ -98,11 +126,12 @@ int verify_list(struct my_struct* head, long thread_num, int list_quota)
 	}
 	for (long i = 0; i < thread_num; i++) {
 		if (entry_counters_by_owner[i] != list_quota) {
-			fprintf(stderr, "Owner %u written %d elements, %d expected\n",
+			fprintf(stderr, "Owner %lu written %d elements, %d expected\n",
 					i, entry_counters_by_owner[i], list_quota);
 			return 1;
 		}
 	}
+	return 0;
 }
 
 // \brief prints list to stdout
