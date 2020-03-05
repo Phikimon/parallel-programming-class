f4ecfa76073cb6462ef611a68ed16b5587b3ae02
Filipp Mikoian <Filipp.Mikoian@acronis.com>
2020-03-04 22:04:33 -0500

    S2T1: add rescription as well as base file list.h

---
+++ seminar_2/README.md
@@ -0,0 +1,3 @@
+## Task
+
+With the use of compare and swap implement single-linked list that could be used in parallel. Only reading and insertion need to be supported, so we do not care about ABA as well as performance.
---
+++ seminar_2/list.h
@@ -0,0 +1,82 @@
+#ifndef __LIST_H
+#define __LIST_H
+
+struct list_head {
+    struct list_head *next;
+};
+
+#define LIST_HEAD_INIT(name) { &(name) }
+
+#define LIST_HEAD(name) \
+    struct list_head name = LIST_HEAD_INIT(name)
+
+#define INIT_LIST_HEAD(ptr) do { \
+    (ptr)->next = (ptr); \
+} while (0)
+
+/*
+ * Insert a new entry between two known consecutive entries.
+ *
+ * This is only for internal list manipulation where we know
+ * the prev/next entries already!
+ */
+static inline void __list_add(struct list_head *new,
+                  struct list_head *prev,
+                  struct list_head *next)
+{
+    new->next = next;
+    prev->next = new;
+}
+
+/**
+ * list_add - add a new entry
+ * @new: new entry to be added
+ * @head: list head to add it after
+ *
+ * Insert a new entry after the specified head.
+ * This is good for implementing stacks.
+ */
+static inline void list_add(struct list_head *new, struct list_head *head)
+{
+    __list_add(new, head, head->next);
+}
+
+/**
+ * list_empty - tests whether a list is empty
+ * @head: the list to test.
+ */
+static inline int list_empty(struct list_head *head)
+{
+    return head->next == head;
+}
+
+/**
+ * list_entry - get the struct for this entry
+ * @ptr:    the &struct list_head pointer.
+ * @type:    the type of the struct this is embedded in.
+ * @member:    the name of the list_struct within the struct.
+ */
+#define list_entry(ptr, type, member) \
+    ((type *)((char *)(ptr)-(unsigned long)(&((type *)0)->member)))
+
+/**
+ * list_for_each    -    iterate over a list
+ * @pos:    the &struct list_head to use as a loop counter.
+ * @head:    the head for your list.
+ */
+#define list_for_each(pos, head) \
+    for (pos = (head)->next; pos != (head); \
+            pos = pos->next)
+
+/**
+ * list_for_each_entry    -    iterate over list of given type
+ * @pos:    the type * to use as a loop counter.
+ * @head:    the head for your list.
+ * @member:    the name of the list_struct within the struct.
+ */
+#define list_for_each_entry(pos, head, member)                \
+    for (pos = list_entry((head)->next, typeof(*pos), member);    \
+         &pos->member != (head);                     \
+         pos = list_entry(pos->member.next, typeof(*pos), member))
+
+#endif
